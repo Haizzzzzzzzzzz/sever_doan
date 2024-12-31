4b951dc4821d0bea121605bf56106897fb956129
Haizzzzzzzzzzz <112196630+Haizzzzzzzzzzz@users.noreply.github.com>
2024-12-31 14:24:43 +0700

    Update plugins.js
    để có url ảnh chuẩn giống như code ở local

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      providerOptions: {
+        url: "/uploads", // Đảm bảo URL của ảnh sẽ là dạng tương đối
+      },
+    },
+  },
+});
